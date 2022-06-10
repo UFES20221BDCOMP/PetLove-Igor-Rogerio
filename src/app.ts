fa30fe23fc69e24854dec0832359e28f016a1a3e
IgorDummer <igorwdummer@outlook.com>
2022-06-10 17:29:30 -0300

    alterando app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  return response.json({ message: '🧌 Server is run on port 4000' })
+})
+
 app.listen(4000, () => console.log('🧌 Server is run on port 4000'))
